84af7bc5cc02e3d9171511ec753751d21adb38e7
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-02-08 20:05:22 +0700

    feat(project): This is Future shop with fetch api . then ().then to use another open api resourece from dummyJSON

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-// const attachedCard = (productCard) => {
-// }
-const createCard = () => {
+let products = []
+const cart = {}
+
+const updateCart = () => {
+  let totalPrice = 0;
+  document.querySelector('#cartSummary_items').replaceChildren([]);
+  for (const key of Object.keys(cart)) {
+    const item = products.find((product) => `${product.id}` === key)
+    const quantity = cart[key]
+    const price = item.price
+
+    const itemRow = document.createElement('tr')
+    const itemName = document.createElement('td')
+    itemName.innerText = item.title
+
+    const itemQuantity = document.createElement('td')
+    itemQuantity.innerText = quantity
+
+    const itemPrice = document.createElement('td')
+    itemPrice.innerText = quantity * price
+
+    itemRow.append(itemName, itemQuantity, itemPrice)
+    document.querySelector('#cartSummary_items').appendChild(itemRow)
+
+    if (quantity === 0){
+      document.querySelector('#cartSummary_items').removeChild(itemRow)
+    }
+
+    totalPrice += (price * quantity)
+  }
+  document.querySelector('#cartSummary_total').innerText = totalPrice.toFixed(2);
+}
+
+const createCard = (product) => {
   const productCard = document.createElement('div')
   productCard.className = 'productCard'
 
   const productThumbnail = document.createElement('img')
   productThumbnail.className = 'productThumbnail'
-  productThumbnail.src =
-    'https://cdn.dummyjson.com/products/images/beauty/Red%20Nail%20Polish/1.png'
+  productThumbnail.src = product.thumbnail
 
   const productBottomSheet = document.createElement('div')
   productBottomSheet.className = 'productBottomSheet'
@@ -17,23 +47,70 @@ const createCard = () => {
 
   const productName = document.createElement('strong')
   productName.className = 'productName'
-  productName.innerText = 'iPhone X'
+  productName.innerText = product.title
 
   const productPrice = document.createElement('div')
   productPrice.className = 'productPrice'
-  productPrice.innerText = '$100'
+  productPrice.innerText = `$${product.price.toFixed(2)}`
+
+  const productButton = document.createElement('div')
+  productButton.className = 'productButton'
 
   const addToCard = document.createElement('button')
-  addToCard.className = 'addToCard'
+  addToCard.className = 'addToCart'
   addToCard.innerText = '+'
 
+  const removeToCart = document.createElement('button')
+  removeToCart.className = 'removeToCart'
+  removeToCart.innerText = '-'
+
+  addToCard.addEventListener('click', () => {
+    // {}
+    if (cart[product.id] === undefined) cart[product.id] = 0
+    cart[product.id] += 1
+    updateCart()
+  })
+
+  removeToCart.addEventListener('click', () => {
+    // {}
+    if (cart[product.id]) {
+      cart[product.id] -= 1
+    }
+    updateCart()
+  })
+
   productInfoContainer.append(productName, productPrice)
-  productBottomSheet.append(productInfoContainer, addToCard)
+  productButton.append(addToCard, removeToCart)
+  productBottomSheet.append(productInfoContainer, productButton)
   productCard.append(productThumbnail, productBottomSheet)
 
   document.querySelector('#productList').appendChild(productCard)
 }
 
-for(let i=0;i<10;i++){
-  createCard()
+const fetchProducts = () => {
+  fetch('https://dummyjson.com/products')
+    .then((res) => res.json())
+    .then((productsResponse) => {
+      products = productsResponse.products
+      products.forEach((product) => {
+        createCard(product)
+      })
+    })
 }
+
+const hookViewCart = () =>{
+  const viewCartButton = document.querySelector('#viewCart');
+  viewCartButton.addEventListener('click', ()=>{
+    const cartSummary = document.querySelector('#cartSummary')
+    const display = cartSummary.style.display
+
+    if (display === 'none'){
+      cartSummary.style.display = 'block'
+    }else{
+      cartSummary.style.display = 'none'
+    }
+  })
+}
+
+fetchProducts();
+hookViewCart();
